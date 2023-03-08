d92c274f44715dd22acbabe556caedb7d4024de0
Nina Saalfrank <123561210+salaos@users.noreply.github.com>
2023-03-08 13:04:05 +0100

    change entry form

--- src/components/EntryForm/EntryForm.js
+++ src/components/EntryForm/EntryForm.js
@@ -1,25 +1,31 @@
 import Button from "../Button/Button";
+import React from "react";
+import { useState } from "react";
+import { uid } from "uid";
 
-export default function EntryForm({ onAddEntry }) {
-  function handleSubmit(event) {
+export default function EntryForm({ onAddActivity }) {
+  const [activityName, setActivityName] = useState("");
+  const [isForGoodWeather, setIsForGoodWeather] = useState("false");
+
+  function handleSubmit(event) => {
     event.preventDefault();
-    const formData = new FormData(event.target);
-    const data = Object.fromEntries(formData);
-    onAddEntry(data);
+    const newActivity = { name: activityName, id: uid() };
     event.target.reset();
   }
+
+  onAddActivity();
 
   return (
     <form className="entry-form" onSubmit={handleSubmit}>
       <h2 className="entry-form__title">Add new activity:</h2>
       <div className="entry-form__fields">
         <div className="entry-form__field">
-          <label htmlFor="motto">Name</label>
-          <input type="text" name="motto" id="motto" />
+          <label htmlFor="activityName">Name</label>
+          <input type="text" id="activityName" />
         </div>
         <div className="entry-form__field">
-          <label htmlFor="activity">Good-weather activity</label>
-          <input type="checkbox" id="activity"></input>
+          <label htmlFor="isForGoodWeather">Good-weather activity</label>
+          <input type="checkbox" id="isForGoodWeather"></input>
         </div>
         <div className="entry-form__button-wrapper">
           <Button type="submit">Submit</Button>
