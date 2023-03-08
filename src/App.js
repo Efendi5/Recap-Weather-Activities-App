e8fc7bd0d2765337e04c8ff7ece43580cba5ed43
Nina Saalfrank <123561210+salaos@users.noreply.github.com>
2023-03-08 16:23:54 +0100

    add api

--- src/App.js
+++ src/App.js
@@ -4,11 +4,30 @@ import useLocalStorageState from "use-local-storage-state";
 import { uid } from "uid";
 import List from "./components/List/List";
 import Entry from "./components/Entry/Entry";
+import { useEffect, useState } from "react";
 
 function App() {
   const [entries, setEntries] = useLocalStorageState("weather-entries", {
     defaultValue: [],
   });
+  const weather = useWeatherApi(`https://example-apis.vercel.app/api/weather`);
+
+  function useWeatherApi(url) {
+    const [isGoodWeather, setIsGoodWeather] = useState();
+
+    useEffect(() => {
+      async function startFetching() {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        setIsGoodWeather(data.isGoodWeather);
+      }
+
+      startFetching();
+    }, [url]);
+    console.log(isGoodWeather);
+    return isGoodWeather;
+  }
 
   function handleAddEntry(newEntry) {
     setEntries([
@@ -24,9 +43,11 @@ function App() {
   console.log(entries);
   return (
     <div>
-      <List isGoodWeather={true}>
+      <List isGoodWeather={!weather}>
         {entries
-          .filter((entry) => entry.isGoodWeather)
+          .filter((entry) =>
+            weather ? entry.isGoodWeather : !entry.isGoodWeather
+          )
           .map((entry) => {
             return <Entry key={entry.id} description={entry.name} />;
           })}
